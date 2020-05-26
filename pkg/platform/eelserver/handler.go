65fce41204fafcc6499ed8c97a5a0f20e3798cee
John Bramlett <jbramlett@3street.net>
2020-05-25 20:44:38 -0400

    Simplifying http client request

--- pkg/platform/eelserver/handler.go
+++ pkg/platform/eelserver/handler.go
@@ -2,13 +2,8 @@ package eelserver
 
 import (
 	"context"
-	"encoding/json"
-	"net/http"
-	"time"
 
-	"github.com/jfbramlett/go-aop/pkg/tracing"
-	"github.com/jfbramlett/go-aop/pkg/web"
-
+	"github.com/jfbramlett/go-aop/pkg/rest"
 	"github.com/jfbramlett/nwp-platform-go/pkg/eelmodel"
 
 	"github.com/jfbramlett/go-aop/pkg/aop"
@@ -25,21 +20,7 @@ func (e *EELHandler) GetAccountList(ctx context.Context, req *eelmodel.AccountLi
 	aspectCtx := aop.Before(ctx)
 	defer func() { aop.After(aspectCtx, err) }()
 
-	traceId := tracing.GetTraceFromContext(aspectCtx)
-
-	httpReq, _ := http.NewRequest("GET", "http://localhost:8090/accountlist", nil)
-	httpReq.Header.Set(web.HeaderRequestId, traceId)
-
-	client := &http.Client{Timeout: time.Second * 10}
-
-	htmlResp, err := client.Do(httpReq)
-	if err != nil {
-		return nil, err
-	}
-	defer htmlResp.Body.Close()
 	resp = &eelmodel.AccountListResponse{}
-	decoder := json.NewDecoder(htmlResp.Body)
-	err = decoder.Decode(&resp)
-
+	err = rest.HttpGetRequest(aspectCtx, "http://localhost:8090/accountlist", &resp)
 	return resp, err
 }
